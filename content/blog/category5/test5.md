506e534122779caa50cea7a5975316ba44610202
ChulJin Lim <cjfwls9806@gmail.com>
2023-09-04 19:39:39 +0900

    init: init blog

---
+++ content/blog/category5/test5.md
@@ -0,0 +1,8 @@
+---
+title: 'Category Test5'
+date: 2020-07-02 16:21:13
+category: 'category5'
+draft: false
+---
+
+Test1
---
+++ gatsby-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: `imironjin's Blog`,
+  description: `Blog posted about ...`,
+  author: `imironjin`,
+  introduction: `Web Front-End Developer`,
+  siteUrl: `https://imironjin.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `imironjin`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'imironjin/imironjin-blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}
