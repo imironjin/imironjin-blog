e042aaef4d9188d81398a0e51c5c07485c53a4c6
ChulJin Lim <cjfwls9806@gmail.com>
2023-09-04 22:12:13 +0900

    design: add date to post thumbnail

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -8,6 +8,7 @@ export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
+      <time dateTime={node.frontmatter.date}>📆{node.frontmatter.date}</time>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
